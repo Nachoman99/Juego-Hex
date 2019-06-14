2d248dfdbab03e145f98c870ebfb8dacee2d9e47
Kevin Trejos <Kevin Trejos@192.168.0.13>
2019-06-13 23:40:57 -0600

    Ventanas Connecting y waitConnection

--- Juego Hex/src/Sockets/Client.java
+++ Juego Hex/src/Sockets/Client.java
@@ -5,11 +5,48 @@
  */
 package Sockets;
 
+import GUI.VentanaPrincipal;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  *
  * @author Kevin Trejos
  */
 public class Client {
     
+    private ObjectInputStream input;
+    private ObjectOutputStream output;
+    private Socket client;
+    private final String HOST = "127.0.0.1";
+    private final int PORT = 12345;
+    private VentanaPrincipal mainWindow;
     
+    public void runClient(){
+        
+    }
+    
+    private void connectToServer() throws IOException{
+        System.out.println("Attempting connection\n");
+        client = new Socket(HOST, PORT);
+        System.out.println("Connected to: " + client.getInetAddress().getHostName());
+    }
+    
+    private void getStreams() throws IOException{
+        output = new ObjectOutputStream(client.getOutputStream());
+        output.flush();
+        input = new ObjectInputStream(client.getInputStream());
+    }
+    
+    private void processConnection(){
+        
+    }
+    
+    private void closeConnection(){
+        
+    }
 }
